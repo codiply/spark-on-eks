4e73a929807add1c4950efad31b69ab536404792
Panagiotis Katsaroumpas <codiply@gmail.com>
2021-08-17 16:12:00 +0200

    Set my IP address automatically

--- infrastructure/cdk.sh
+++ infrastructure/cdk.sh
@@ -2,4 +2,10 @@
 
 set -euo pipefail
 
+MY_IP=$(curl -s ifconfig.me/ip)
+
+for filename in ./config/*.deployment.yaml; do
+  sed -i '' -E "s/- '[^']*' # AUTO_FILL_MY_IP_ADDRESS/- '$MY_IP\/32' # AUTO_FILL_MY_IP_ADDRESS/g" $filename
+done
+
 cdk --profile $(cat ./config/aws-profile.txt) "${@}"
